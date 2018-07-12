19750a1501df240b55afd0d2637fa6d6f9b00668
leofavre <contato@leofavre.com>
2018-07-11 21:55:22 -0300

    Add test configuration

---
+++ karma.conf.js
@@ -0,0 +1,51 @@
+const path = require('path');
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
+const filesPattern = '*.test.js';
+
+module.exports = (config) => {
+  config.set({
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
+    browserNoActivityTimeout: 60000,
+    singleRun: true,
+    frameworks: ['mocha', 'chai'],
+    files: [{
+      pattern: filesPattern,
+      watched: false
+    }],
+    preprocessors: {
+      [filesPattern]: ['webpack']
+    },
+    webpack: {
+      mode: 'development',
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            use: {
+              loader: 'istanbul-instrumenter-loader',
+              options: {
+                esModules: true
+              }
+            },
+            exclude: /((node_modules)(\\|\/|$)|test.js$)/
+          }
+        ]
+      }
+    },
+    mochaReporter: {
+      showDiff: true
+    },
+    reporters: ['mocha', 'coverage-istanbul'],
+    coverageIstanbulReporter: {
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
+    }
+  });
+};
