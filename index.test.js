6c0006eddfded252704cc024590ef6c83935279e
leofavre <contato@leofavre.com>
2018-07-11 22:40:18 -0300

    Add a simple test to check if Travis is working

---
+++ index.test.js
@@ -0,0 +1,7 @@
+import { withObservedProperties } from './index.js';
+
+describe('withObservedProperties', () => {
+  it('Should not be undefined.', () => {
+    expect(withObservedProperties).to.not.be.undefined;
+  });
+});
