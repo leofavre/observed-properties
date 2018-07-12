eda811a2a97927d7fc5123827239c7618db05abf
leofavre <contato@leofavre.com>
2018-07-12 00:11:18 -0300

    Add more tests

--- index.test.js
+++ index.test.js
@@ -1,7 +1,41 @@
 import { withObservedProperties } from './index.js';
+import chai from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+chai.use(sinonChai);
+
+class RateComponent extends withObservedProperties() {
+  static get observedProperties () {
+    return ['rate'];
+  }
+}
+
+window.customElements.define('observed-rate', RateComponent);
+window.customElements.define('unobserved-rate', withObservedProperties());
 
 describe('withObservedProperties', () => {
-  it('Should not be undefined.', () => {
-    expect(withObservedProperties).to.not.be.undefined;
+  it('Should not trigger propertyChangedCallback when ' +
+    'there are no observed properties.', () => {
+    const testElement = document.createElement('unobserved-rate');
+    testElement.propertyChangedCallback = sinon.spy();
+    testElement.rate = 50;
+
+    expect(testElement.propertyChangedCallback).not.to.have.been.called;
+  });
+
+  it('Should trigger propertyChangedCallback when ' +
+    'an observed property changes.', () => {
+    const testElement = document.createElement('observed-rate');
+    testElement.propertyChangedCallback = sinon.spy();
+    testElement.rate = 50;
+
+    expect(testElement.propertyChangedCallback).to.have.been
+      .calledWith('rate', undefined, 50);
+  });
+
+  it('Should make getters for the observed properties.', () => {
+    const testElement = document.createElement('observed-rate');
+    testElement.rate = 50;
+    expect(testElement.rate).to.equal(testElement._rate);
   });
 });
