c9b28e0504313bbf8c37290caa41733066841c83
leofavre <contato@leofavre.com>
2018-07-14 12:09:39 -0300

    Fix propertyChangedCallback behaviour

--- index.js
+++ index.js
@@ -2,25 +2,26 @@ export const withObservedProperties = (Base = HTMLElement) =>
   class extends Base {
     constructor () {
       super();
+      const { propertyChangedCallback } = this;
       const { observedProperties = [] } = this.constructor;
-      observedProperties.forEach(propName => {
-        const inheritedValue = this[propName];
-        const privateKey = Symbol(propName);
-        this[privateKey] = inheritedValue;
 
-        Object.defineProperty(this, propName, {
-          get () {
-            return this[privateKey];
-          },
-          set (value) {
-            const oldValue = this[privateKey];
-            this[privateKey] = value;
+      if (typeof propertyChangedCallback === 'function') {
+        observedProperties.forEach(propName => {
+          const inheritedValue = this[propName];
+          const privateKey = Symbol(propName);
+          this[privateKey] = inheritedValue;
 
-            if (typeof this.propertyChangedCallback === 'function') {
-              this.propertyChangedCallback(propName, oldValue, value);
+          Object.defineProperty(this, propName, {
+            get () {
+              return this[privateKey];
+            },
+            set (value) {
+              const oldValue = this[privateKey];
+              this[privateKey] = value;
+              propertyChangedCallback(propName, oldValue, value);
             }
-          }
+          });
         });
-      });
+      }
     }
   };
